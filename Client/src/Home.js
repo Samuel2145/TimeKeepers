8f8fec1475bd8f71c366ec5ad250a228275edfdf
Sam Bevans-Kerr <sambk@MacBook-Pro.local>
2021-02-26 11:55:25 -0500

    Some futile home changes

--- Client/src/Home.js
+++ Client/src/Home.js
@@ -4,16 +4,25 @@ import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import NavItem from 'react-bootstrap/NavItem';
+import CardColumns from 'react-bootstrap/CardColumns';
+
+import bigthink from './img/bigthink.png';
+
+const divStyle = {
+  paddingTop: '10px',
+};
 
 class Home extends Component {
   render() {
     return (
-    
-      <Container fluid>
-        <Row>
 
-       <Card style={{ width: '28rem' }}>
+      <Container fluid>
+        <Row className="justify-content-md-end">
+          <Col>
+       <Card style={{ width: '18rem' } }>
+       <Card.Img variant="top" src={bigthink} />
         <Card.Body>
         <Card.Title>Employee Login</Card.Title>
         <Card.Text>
@@ -25,8 +34,11 @@ class Home extends Component {
         
       </Card.Body>
     </Card>
+    </Col>
 
-    <Card style={{ width: '28rem' }}>
+      <Col>
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={bigthink} />
         <Card.Body>
         <Card.Title>Admin Login</Card.Title>
         <Card.Text>
@@ -36,15 +48,7 @@ class Home extends Component {
       </Card.Body>
     </Card>
 
-    <Card style={{ width: '28rem' }}>
-        <Card.Body>
-        <Card.Title>Calendar</Card.Title>
-        <Card.Text>
-          Check the current work schedule.
-        </Card.Text>
-        <Button variant="primary">Login</Button>
-      </Card.Body>
-    </Card>
+    </Col>
       </Row>
 
       </Container>
