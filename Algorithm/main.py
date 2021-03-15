78ba3a62535931c1ebc7952e1ab3b1aa34a40e24
wpascuzzi <wpascuzzi@ufl.edu>
2021-03-15 12:07:06 -0400

    adding functionality to send shifts back to database

--- Algorithm/main.py
+++ Algorithm/main.py
@@ -1,3 +1,6 @@
+import math
+import time
+import mariadb
 from CH import buildSchedule
 from LocalSearch import localSearch
 from collections import OrderedDict
@@ -7,9 +10,23 @@ import numpy as np
 import json
 import sys
 
+
+
+
+def getConnection():
+    user = "n9qa33fb24h5ojln"
+    password = 'w5ie9n0wkv2mlpvs'
+    host = "ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
+    port = 3306
+    database = 'zry2wsgus6t4stzn'
+
+    connection = mariadb.connect(user=user, password=password, host=host, port=port, database=database)
+    return connection
+
 # need to receive:
 # employee roster w/ availabilities for each employee
 # parameters
+
 
 data = json.load(sys.stdin)
 
@@ -24,8 +41,24 @@ constraints = Constraints.Constraints(shiftLengths, schedStart, schedEnd, 1, ((s
 roster = OrderedDict({entry['username'] : Employee(entry['username'] ,entry['avails'] ) for entry in data['employees']})
 
 sched = buildSchedule(roster, constraints)
-sched.displaySchedule()
+#sched.displaySchedule()
 print("")
 sched = localSearch(sched)
 
-sched.displaySchedule()
+#sched.displaySchedule()
+
+connection = getConnection()
+
+#this is temporary for the purposes of testing. 
+#TODO: give shift object a full datetime field.
+d = 15
+cursor = connection.cursor()
+for day in sched.schedule:    
+    for shift in sched.schedule[day]:
+        start = ("2021-03-%d %H:%M:00", d, math.floor(shift.shiftStart/2), (shift.shiftStart%2)*30)
+        end = ("2021-03-%d %H:%M:00", d, math.floor(shift.shiftEnd/2), (shift.shiftEnd%2)*30)
+        print(start)
+        print(end)
+        #cursor.execute("INSERT INTO shift (username, start, end) VALUES (?, ?, ?)",         
+        #(shift.employeeID, start, end))
+    d += 1
