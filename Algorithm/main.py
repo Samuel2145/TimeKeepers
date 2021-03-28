d2bcc77c5e032ca72a52b2751e3649b912a6fef7
wpascuzzi <wpascuzzi@ufl.edu>
2021-03-27 20:09:07 -0400

    allows shifts to be sent to database

--- Algorithm/main.py
+++ Algorithm/main.py
@@ -1,6 +1,8 @@
+import datetime
 import math
 import time
 import mariadb
+from datetime import date
 from CH import buildSchedule
 from LocalSearch import localSearch
 from collections import OrderedDict
@@ -9,9 +11,6 @@ import Constraints
 import numpy as np
 import json
 import sys
-
-
-
 
 def getConnection():
     user = "n9qa33fb24h5ojln"
@@ -30,7 +29,7 @@ def getConnection():
 
 data = json.load(sys.stdin)
 
-print(data)
+#print(data)
 
 schedStart = 10
 schedEnd = 35
@@ -48,17 +47,24 @@ sched = localSearch(sched)
 #sched.displaySchedule()
 
 connection = getConnection()
+try:
+    #this is temporary for the purposes of testing. 
+    #TODO: give shift object a full datetime field.
+    #today = datetime.today()
+    d = 15
+    
+    cursor = connection.cursor()
+    for day in sched.schedule:    
+        for shift in sched.schedule[day]:
+            start = "2021-03-%02d %02d:%02d:00" %( d, math.floor(shift.shiftStart/2), (shift.shiftStart%2)*30)
+            end = "2021-03-%02d %02d:%02d:00" %(d, math.floor(shift.shiftEnd/2), (shift.shiftEnd%2)*30)
+            #print(start)
+            #print(end)
+            cursor.execute("INSERT INTO shift (username, start, end) VALUES (?, ?, ?)",         
+            (shift.employeeID, start, end))
+        d += 1
+except mariadb.Error as e:
+    print(f"Error: {e}")
 
-#this is temporary for the purposes of testing. 
-#TODO: give shift object a full datetime field.
-d = 15
-cursor = connection.cursor()
-for day in sched.schedule:    
-    for shift in sched.schedule[day]:
-        start = ("2021-03-%d %H:%M:00", d, math.floor(shift.shiftStart/2), (shift.shiftStart%2)*30)
-        end = ("2021-03-%d %H:%M:00", d, math.floor(shift.shiftEnd/2), (shift.shiftEnd%2)*30)
-        print(start)
-        print(end)
-        #cursor.execute("INSERT INTO shift (username, start, end) VALUES (?, ?, ?)",         
-        #(shift.employeeID, start, end))
-    d += 1
+connection.commit() 
+connection.close()
