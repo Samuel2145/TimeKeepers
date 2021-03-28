b667de8bd804369e7ecdfc1fcb7c0932f7a722c4
Samuel2145 <samuelssj4.02@gmail.com>
2021-03-28 00:08:12 -0400

    Check this out Will

--- Algorithm/main.py
+++ Algorithm/main.py
@@ -1,7 +1,7 @@
 import datetime
 import math
 import time
-import mariadb
+#import mariadb
 from datetime import date
 from CH import buildSchedule
 from LocalSearch import localSearch
@@ -12,15 +12,15 @@ import numpy as np
 import json
 import sys
 
-def getConnection():
-    user = "n9qa33fb24h5ojln"
-    password = 'w5ie9n0wkv2mlpvs'
-    host = "ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
-    port = 3306
-    database = 'zry2wsgus6t4stzn'
+#def getConnection():
+#    user = "n9qa33fb24h5ojln"
+#    password = 'w5ie9n0wkv2mlpvs'
+#    host = "ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
+#    port = 3306
+#    database = 'zry2wsgus6t4stzn'
 
-    connection = mariadb.connect(user=user, password=password, host=host, port=port, database=database)
-    return connection
+#    connection = mariadb.connect(user=user, password=password, host=host, port=port, database=database)
+#    return connection
 
 # need to receive:
 # employee roster w/ availabilities for each employee
@@ -44,27 +44,27 @@ sched = buildSchedule(roster, constraints)
 print("")
 sched = localSearch(sched)
 
-#sched.displaySchedule()
+sched.displaySchedule()
 
-connection = getConnection()
-try:
+#connection = getConnection()
+#try:
     #this is temporary for the purposes of testing. 
     #TODO: give shift object a full datetime field.
     #today = datetime.today()
-    d = 15
+ #   d = 15
     
-    cursor = connection.cursor()
-    for day in sched.schedule:    
-        for shift in sched.schedule[day]:
-            start = "2021-03-%02d %02d:%02d:00" %( d, math.floor(shift.shiftStart/2), (shift.shiftStart%2)*30)
-            end = "2021-03-%02d %02d:%02d:00" %(d, math.floor(shift.shiftEnd/2), (shift.shiftEnd%2)*30)
+  #  cursor = connection.cursor()
+  #  for day in sched.schedule:
+  #      for shift in sched.schedule[day]:
+  #          start = "2021-03-%02d %02d:%02d:00" %( d, math.floor(shift.shiftStart/2), (shift.shiftStart%2)*30)
+  #          end = "2021-03-%02d %02d:%02d:00" %(d, math.floor(shift.shiftEnd/2), (shift.shiftEnd%2)*30)
             #print(start)
             #print(end)
-            cursor.execute("INSERT INTO shift (username, start, end) VALUES (?, ?, ?)",         
-            (shift.employeeID, start, end))
-        d += 1
-except mariadb.Error as e:
-    print(f"Error: {e}")
+  #          cursor.execute("INSERT INTO shift (username, start, end) VALUES (?, ?, ?)",
+  #          (shift.employeeID, start, end))
+  #      d += 1
+#except mariadb.Error as e:
+#    print(f"Error: {e}")
 
-connection.commit() 
-connection.close()
+#connection.commit()
+#connection.close()
