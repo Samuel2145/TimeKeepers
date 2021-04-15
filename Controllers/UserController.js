d879481b32849e9bc4dd018a8c888cb9e29a31de
Samuel2145 <samuelssj4.02@gmail.com>
2021-04-15 18:05:19 -0400

    Code clean up and standardization
    Altered query to use structure we've been using throughout

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -510,18 +510,21 @@ export const getCalendar = (req,res) => {
     let searchQ;
     let params = [];
     let curr = new Date(req.body.curr);
-    var weekStartDate = new Date(curr.setDate(curr.getDate() - curr.getDay()));
-    var weekStart = weekStartDate.getFullYear() + "-" + (parseInt(weekStartDate.getMonth())+1) + "-" + parseInt(weekStartDate.getDate());
-    var weekEndDate = new Date(curr.setDate(curr.getDate() - curr.getDay()+6));
-    var weekEnd = weekEndDate.getFullYear() + "-" + (parseInt(weekEndDate.getMonth())+1) + "-" +parseInt(weekEndDate.getDate());
+    let weekStartDate = new Date(curr.setDate(curr.getDate() - curr.getDay()));
+    let weekStart = weekStartDate.getFullYear() + "-" + (parseInt(weekStartDate.getMonth())+1) + "-" + parseInt(weekStartDate.getDate());
+    let weekEndDate = new Date(curr.setDate(curr.getDate() - curr.getDay()+6));
+    let weekEnd = weekEndDate.getFullYear() + "-" + (parseInt(weekEndDate.getMonth())+1) + "-" +parseInt(weekEndDate.getDate());
 
     if(userData.isEmployer === 1){
         params.push(userData.Group)
-        searchQ = `SELECT username, start, end FROM shift WHERE parameterID=(SELECT parameterID FROM parameter WHERE groupName=?) AND start BETWEEN '${weekStart}' AND '${weekEnd}' ORDER BY username ASC`;
+        searchQ = "SELECT username, start, end, FROM shift WHERE parameterID=(SELECT parameterID FROM parameter WHERE groupName=?) AND start BETWEEN ? AND ? ORDER BY username ASC";
     }else{
         params.push(userData.username);
-        searchQ = `SELECT username, start, end FROM shift WHERE username=? AND start BETWEEN '${weekStart}' AND '${weekEnd}'` ;
+        searchQ = "SELECT username, start, end FROM shift WHERE username=? AND start BETWEEN ? AND ?"
     }
+
+    params.push(weekStart);
+    params.push(weekEnd);
 
     conn.query(searchQ, params,(err,result) => {
 
